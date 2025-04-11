694ffd7b1d84e557d3438d19e0614f7ab4871d4a
Pamela4590 <abatonipamela75@gmail.com>
2025-04-11 20:57:23 +0200

    fixed

--- src/Dashboard/Charts.jsx
+++ src/Dashboard/Charts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 
+
 const data = [
   { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
   { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
